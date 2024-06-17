8a481cb18890e41eac5282e32ba44df25f585767
danniserena <15113078698@163.com>
2024-06-18 04:40:07 +0800

    Call function from module library using libdl.so
    1. Add check file in src/Makefile.am
    
    2. Use `-module` to skip `lib` prefix and `-avoid-version` to skip libtool version suffix
    when generate module library. Notice that the location prefix of a module is `pkglib`, and
    a convenience library one is `noinst`, and a public library one is `lib`
    
    3. Also, notice that public interface use `#include<>` while private
    one use `#include ""`

--- src/main.c
+++ src/main.c
@@ -1,10 +1,42 @@
 #include <libjupiter.h>
+#include <stdio.h>
+#include "module.h"
+
+#ifdef HAVE_CONFIG_H
+# include <config.h>
+#endif
+
+#if HAVE_DLFCN_H
+# include <dlfcn.h>
+#endif
 
 #define DEFAULT_SALUTATION "Hello"
 
 int main(int argc, char * argv[])
 {
   const char* salutation = DEFAULT_SALUTATION;
+#if HAVE_DLFCN_H
+  void * module;
+  get_salutation_t* get_salutation_fp = 0;
+
+  module = dlopen("./module.so", RTLD_NOW);
+  if (module != 0) {
+    get_salutation_fp = (get_salutation_t *)dlsym(module, GET_SALUTATION_SYM);
+    if (get_salutation_fp != 0)
+      salutation = get_salutation_fp();
+    else
+      printf("no %s function in module.so\n", GET_SALUTATION_SYM);
+  }else {
+    printf("no module.so file found\n");
+  }
+#endif
+
  jupiter_print(salutation, argv[0]);
+
+#if HAVE_DLFCN_H
+ if (module != 0)
+   dlclose(module);
+#endif
+
  return 0;
 }
